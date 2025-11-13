6018378cb914e7f7c2eff6418eb67e62c512c873
Ayush Jain <ayushjainjbp2011@gmail.com>
2025-11-13 10:25:58 +0530

    adding sum.py

--- sum.py
+++ sum.py
@@ -4,3 +4,4 @@ duplicate_item = [item for item,count in count.items() if count > 1]
 duplicate_with_count ={item:count for item,count in count.items() if count > 1}
 print(duplicate_item) #[1, 2]
 print(duplicate_with_count) #{1: 2, 2: 2}
+print("changes done after initial commit")
